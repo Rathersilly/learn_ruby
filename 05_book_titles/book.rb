08a5841401fe491373488196db213f00f267f6fd
Rathersilly <rathersilly@live.com>
2017-05-03 13:25:26 -0600

    05 done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,35 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def initialize
+	end
+
+	def title=(name)
+		@title = titleize(name)
+		
+	end
+
+	private
+	def titleize(string)
+		ignorelist = %w[the and over in of a an]
+		words = string.split(" ")
+		temparray = []
+		words.each do |i|
+			i = i.capitalize if !ignorelist.include?(i)
+			temparray.push(i)
+		end
+		temparray[0].capitalize!
+
+		return temparray.join(" ")
+
+	end
+
+
+
 end
+
+book = Book.new
+book.title = "hi"
+puts book.title
+puts book.inspect
